dcabeaec355000a2f51c6ebc07e3d7a510899c09
alphaKAI <alpha.kai.net@alpha-kai-net.info>
2016-09-07 18:06:20 +0900

    automatic scroll when cursur goes out of window

--- source/app.d
+++ source/app.d
@@ -14,9 +14,12 @@ enum y_offset = 1; // for query
 
 struct Env {
   string[] inputs;
+  string[] render_items;
+  string[] filtered;
   size_t query_offset;
   string query;
-  ulong selected;
+  long selected,
+       cursor;
 }
 
 static Env E;
@@ -32,11 +35,11 @@ void print(ulong x, ulong y, string line, bool selected) {
   if (selected) {
     setCell(x.to!int, (y + y_offset).to!int, c, cast(ushort)Color.red, cast(ushort)Color.white);
   } else {
-    setCell(x.to!int, (y + y_offset).to!int, c, cast(ushort)Color.black, cast(ushort)Color.white);
+    setCell(x.to!int, (y + y_offset).to!int, c, cast(ushort)Color.white, cast(ushort)Color.black);
   }
 }
 
-void updateQuery() {
+void updateQuery(bool init = false) {
   string query_header = "QUERY> ";
   string print_query  = query_header ~  E.query;
 
@@ -55,17 +58,25 @@ void updateQuery() {
       setCell(x.to!int, 0, cast(uint)c, cast(ushort)Color.white, cast(ushort)Color.black);
     }
   }
+
+  if (init) {
+    E.filtered = filter;
+    E.render_items = E.filtered;
+    E.selected = 0;
+    E.cursor   = 0;
+  }
 }
 
 void updateAll() {
   clear;
   updateQuery;
 
-  foreach (y, input; filter) {
+  foreach (y, input; E.render_items) {
     foreach (x; width.iota) {
       print(x, y, input, y == E.selected);
     }
   }
+
   flush;
 }
 
@@ -99,6 +110,10 @@ void main() {
   uint[] chs;
   bool selected;
 
+  size_t offset;
+
+  updateQuery(true);
+
   while (!quit) {
     updateAll;
 
@@ -113,14 +128,39 @@ void main() {
       else if (key == BACKSPACE) {
         if (!E.query.empty) {
           E.query = E.query[0..$-1];
+          updateQuery(true);
         }
       }
-      else if (key == ARROW_UP)    { if (E.selected > 0) E.selected--; }
-      else if (key == ARROW_DOWN)  { if (E.selected < E.inputs.length) E.selected++; }
+      else if (key == ARROW_UP)    {
+        if (E.selected > -1) E.selected--;
+        if (E.cursor > 0) E.cursor--;
+
+        if (E.selected == -1) {
+          E.selected++;
+          if (offset > 0) {
+            offset--;
+            E.render_items = E.filtered[offset..$];
+          }
+        }
+      }
+      else if (key == ARROW_DOWN)  {
+        if (E.cursor < E.render_items.length-1) E.cursor++;
+        if ((E.render_items.length < height - 1) && (E.selected < E.render_items.length-1)) E.selected++;
+        else if ((E.render_items.length > height - 1) && (E.selected < height-1)) E.selected++;
+
+        if (E.selected == height - 1) {
+          E.selected--;
+          if (offset < E.filtered.length-1) {
+            offset++;
+            E.render_items = E.filtered[offset..$];
+          }
+        }
+      }
       else {
         // Except special keys(Ex: TAB, DEL)
         if ((kevent.ch != 0) || (key == 32 && kevent.ch == 0)) {
           E.query ~= kevent.ch;
+          updateQuery(true);
         }
       }
     }
