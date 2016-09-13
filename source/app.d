445bd1b52eea840a9d2e3a5101e261dad8a8f24c
kubo39 <hiroki.noda@mixi.co.jp>
2016-09-13 13:43:39 +0900

    Refactor.

--- source/app.d
+++ source/app.d
@@ -159,7 +159,7 @@ void main() {
 
       final switch (key) with (KeyAction) {
         case ENTER:
-          selected = true;
+          selected = E.render_items.empty ? false : true;
           quit     = true;
           break;
         case BACKSPACE:
@@ -207,7 +207,7 @@ void main() {
   }
 
 
-  if (!E.render_items.empty && selected) {
+  if (selected) {
     writeln(E.render_items[E.selected]);
   }
 }
